c86097ebaaec962c211e01bbe5d40939416754c8
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-02-07 23:01:33 +0900

    feat: initial colorscheme

--- colors/srcerite.lua
+++ colors/srcerite.lua
@@ -4,6 +4,15 @@ local groups = require 'highlite.groups'
 
 local g = highlite.groups('default', srcerite.palette)
 
-g.Visual = groups.extend({ reverse = true }, g.Visual)
+if vim.g.srcerite_inverse_visual then
+   g.Visual = { reverse = true }
+end
+if vim.g.srcerite_inverse_search then
+   g.Search = { reverse = true }
+   g.IncSearch = { reverse = true }
+end
+if vim.g.srcerite_inverse_match_paren then
+   g.MatchParen = groups.extend({ reverse = true }, g.MatchParen)
+end
 
 highlite.generate('srcerite', g, srcerite.terminal_palette)
