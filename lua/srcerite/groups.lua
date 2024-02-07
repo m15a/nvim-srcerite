b5c34658984c4519ab768fa1d449bfa113ba5e48
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-02-08 07:09:56 +0900

    fix: tweak palette

--- lua/srcerite/groups.lua
+++ lua/srcerite/groups.lua
@@ -1,7 +1,16 @@
 local groups = require 'highlite.groups'
-local srcerite = require((...):match '(.+)%.[^.]+')
+local palette = require((...):match '(.+)%.[^.]+').palette
 
-local M = require('highlite').groups('default', srcerite.palette)
+local M = require('highlite').groups('default', palette)
+
+-- A couple of tweaks {{{
+
+M.NormalFloat = M.Normal
+M.CursorLine = groups.extend({ bg = palette.bg_contrast_high }, M.CursorLine)
+
+-- }}}
+
+-- Options {{{
 
 if vim.g.srcerite_inverse_visual then
    M.Visual = { reverse = true }
@@ -16,4 +25,8 @@ if vim.g.srcerite_inverse_match_paren then
    M.MatchParen = groups.extend({ reverse = true }, M.MatchParen)
 end
 
+-- }}}
+
 return M
+
+-- vim: fdm=marker
